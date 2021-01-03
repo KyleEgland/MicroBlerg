689a2f1882f7961a30d0465f63921f79284c5a5b
MediocreMarine <kyle.egland@outlook.com>
2021-01-03 15:22:43 -0600

    error handling and log mods

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,10 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import logging
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
 
 
 app = Flask(__name__)
@@ -16,6 +20,43 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
 
+if not app.debug:
+    # email log configuration
+    if app.config["MAIL_SERVER"]:
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr="no-reply@" + app.config["MAIL_SERVER"],
+            toaddrs=app.config["ADMINS"],
+            subject="MicroBlerg Fail :(",
+            credentials=auth,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    # log file configuration
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    file_handler = RotatingFileHandler(
+        "logs/microblerg.log",
+        maxBytes=10240,
+        backupCount=10
+    )
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [%(pathname)s:%(lineno)d]"
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("MicroBlerg startup")
+
 # Importing routes here is a workaround to avoid circular imports; the routes
 # module imports the app variable defined in this script.
-from app import routes, models
+from app import routes, models, errors
--- readme.md
+++ readme.md
@@ -7,3 +7,18 @@ Just another blog web-app created by following a tutorial.
 Manually setting environment variables in PowerShell:
 
 `$env:FLASK_APP = '.\microblerg.py'`
+`$env:FLASK_DEBUG = 1`
+
+List all environment variables
+`dir env:`
+
+## Running SMTP debugging server
+
+Run this command in a separate terminal window:
+
+`(venv) PS C:\workspace\microblerg> python -m smtpd -n -c DebuggingServer localhost:8025`
+
+Set the following environment variables
+`$env:FLASK_DEBUG = 0`
+`$env:MAIL_SERVER = "localhost"`
+`$env:MAIL_PORT = 8025`
